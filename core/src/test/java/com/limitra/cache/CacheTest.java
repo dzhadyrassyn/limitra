15bd50291fced10533ac6e96709a667d321a0db0
Daniyar Zhadyrassyn <zhadyrassyn.daniyar@gmail.com>
2025-08-25 19:29:07 +0500

    Add mock tests for Cache

---
+++ core/src/test/java/com/limitra/cache/CacheTest.java
@@ -0,0 +1,43 @@
+package com.limitra.cache;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CacheTest {
+
+    @Test
+    void putThenGet_returnsValue(){
+
+    }
+
+    @Test
+    void getAbsent_returnsEmpty() {
+
+    }
+
+    @Test
+    void putWithTtl_expiresAfterDeadline() {
+
+    }
+
+    @Test
+    void putEternal_thenPutWithTtl_replaceWithTtl() {
+
+    }
+
+    @Test
+    void putWithTtl_thenPutEternal_clearsTtl() {
+
+    }
+
+    @Test
+    void removeExisting_returnsTrue_andKeyGone() {
+
+    }
+
+    @Test
+    void size_excludesExpiredKeys() {
+
+    }
+}
